214e3bf5b5e4a779cbbf4a861a1a58dddb289ff4
Anthony Corletti <anthcor@gmail.com>
2023-03-11 22:37:04 -0500

    notes

--- pyrocloud/main.py
+++ pyrocloud/main.py
@@ -2,9 +2,10 @@ import os
 
 os.environ["TZ"] = "UTC"
 
-# TODO: create baremetal ec2 instance on aws
-# TODO: provision the vm with firecracker, kernel, and rootfs
-# TODO: create taps for the vms
-# TODO: create network bridges for the vms (host to vm, vm to host, vm to vm)
-# TODO: run healthchecks on the vms (iperf3, ping, etc, apps, etc)
-# TODO: deploy apps to the vm
+# TODO: ✅ create baremetal ec2 instance on aws
+# TODO: provision the vm with firecracker, kernel, and rootfs – via userdata
+# TODO: create taps for the vms - via userdata
+# TODO: create network bridges for the vms (host to vm, vm to host,
+#   vm to vm) – via userdata
+# TODO: run healthchecks on the vms (iperf3, ping, etc, apps, etc) - command
+# TODO: deploy apps to the vm - command

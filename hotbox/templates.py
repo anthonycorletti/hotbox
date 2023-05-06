00763c7e179c29fd58c1023466c4455f583550b5
Anthony Corletti <anthcor@gmail.com>
2023-05-06 19:03:47 -0400

    fix slashes

--- hotbox/templates.py
+++ hotbox/templates.py
@@ -23,7 +23,7 @@ COPY entrypoint /entrypoint
 
 WORKDIR /tmp/overlay/code
 
-RUN {install} \
+RUN {install} \\
     && {build}
 
 CMD ["/bin/bash", "/entrypoint"]
@@ -285,9 +285,9 @@ apt-get remove docker docker-engine docker.io containerd runc -y
 apt-get install ca-certificates curl gnupg -y
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+echo \\
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \\
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \\
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
